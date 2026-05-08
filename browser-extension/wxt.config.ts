effa1c489d6a13a70e4a8316216f6415f1785fab
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2026-05-08 03:51:07 +0300

    🧱 [PROJECT FOUNDATION]: #1,#2 Adding name, description, version and version_name to wxt.config.ts inside manifest.

--- browser-extension/wxt.config.ts
+++ browser-extension/wxt.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: () => ({
+    name: "FINN Lens",
+    description: "Compare and pin FINN cars with smarter insights and cost estimation.",
+    version: "1.0.0", 
+    version_name: "1.0.0",
     host_permissions: ["https://www.finn.com/*"],
     permissions: [
       "tabs",
