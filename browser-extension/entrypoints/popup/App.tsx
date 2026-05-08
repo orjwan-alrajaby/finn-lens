4c425d9b4c59637f533739230a7a724b4befea5f
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2026-05-08 03:41:40 +0300

    🧱 [PROJECT FOUNDATION]: #1,#2 install & setup 'react-hot-toast' and '@heroicons'.

--- browser-extension/entrypoints/popup/App.tsx
+++ browser-extension/entrypoints/popup/App.tsx
@@ -1,11 +1,22 @@
 import "@/assets/tailwind.css";
 import Logo from "/icon/128.png";
+import { BellAlertIcon } from "@heroicons/react/24/outline";
+import toast from "react-hot-toast";
 
 function App() {
   return (
     <div className="h-100 w-100 text-center text-2xl p-10 bg-blue-950 text-white flex flex-col items-center gap-4">
       <img src={Logo} alt="Logo" />
       <h1>WXT + React + TypeScript Starter Project</h1>
+      <button
+        onClick={() => {
+          toast.success("Toast is working successfully!")
+        }}
+        className="flex items-center gap-2 border border-white p-4 rounded-lg cursor-pointer text-lg"
+      >
+        <BellAlertIcon width={24} height={24} />
+        Click to alert!
+      </button>
     </div>
   );
 }
