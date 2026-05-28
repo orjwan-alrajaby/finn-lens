739e6b61364b25d223aaa4808f7417d5a70674bd
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2026-05-28 12:28:09 +0300

    🚗 [User Story] #6 | [resolve coderabbit PR comments]: add 'rel=noopener noreferrer' attribute to prevent opener access

--- browser-extension/entrypoints/popup/App.tsx
+++ browser-extension/entrypoints/popup/App.tsx
@@ -7,7 +7,12 @@ function App() {
     <div className="h-100 w-100 text-center text-2xl p-10 bg-blue-950 text-white flex flex-col items-center gap-4">
       <img src={Logo} alt="Logo" />
       <h1>WXT + React + TypeScript Starter Project</h1>
-      <a href="https://www.finn.com/" target="__blank" className="flex items-center gap-2 border border-white p-4 rounded-lg cursor-pointer text-lg">
+      <a
+        href="https://www.finn.com/"
+        target="__blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 border border-white p-4 rounded-lg cursor-pointer text-lg"
+      >
         <CursorArrowRippleIcon width={24} height={24} />
         Go To FINN!
       </a>
